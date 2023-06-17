30425cf534e5422e02d31afc7a40be3ad5d776e3
niloy143 <niloymahmudapu@gmail.com>
2023-06-18 01:30:04 +0600

    integer multiplication

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-// import multiplication from "./operators/multiplication";
-import addition from "./operators/addition";
-import subtraction from "./operators/subtraction";
+import multiplication from "./operators/multiplication";
+// import addition from "./operators/addition";
+// import subtraction from "./operators/subtraction";
 
-const num1 = "0.03";
-const num2 = "0";
-// console.log(multiplication([num1, num2]))
+const num1 = "912734908712093847239987324058973209845709823475092387459823745098237045889327509837240952039845702938457092834750982374582730948572039845092875098234750982374059";
+const num2 = "20";
 
-// console.log(addition(num1, num2))
-console.log(subtraction(num1, num2));
+console.log(multiplication(num1, num2));
+// console.log(addition(num1, num2));
+// console.log(subtraction(num1, num2));
--- src/operators/multiplication.ts
+++ src/operators/multiplication.ts
@@ -1,8 +1,28 @@
+import addition from "./addition";
+import compareNumber from "./compare-number";
 import digitEqualizer from "./digit-equalizer";
 
 function multiplication(num1: string, num2: string): string {
-    const [n1, n2] = digitEqualizer([num1, num2]);
-    return `${Number(n1) * Number(n2)}`;
+    const [bigNum, smallNum] = compareNumber(num1, num2);
+    const digitMultiplications: string[] = Array(smallNum.length).fill("");
+    for (let i = smallNum.length - 1; i >= 0; i--) {
+        const digit1 = Number(smallNum[i]);
+        if (!digit1) continue;
+        let mulResult = "";
+        let carry = 0;
+        for (let j = bigNum.length - 1; j >= 0; j--) {
+            const digit2 = Number(bigNum[j]);
+            const mul = (digit1 * digit2) + carry;
+            carry = Math.floor(mul / 10);
+            mulResult = mul % 10 + mulResult;
+        }
+        digitMultiplications[digitMultiplications.length - 1 - i] = `${carry || ""}${mulResult}`;
+    }
+    const result = digitMultiplications.reduce((prev, curr, i) => {
+        const currNum = curr + Array(i).fill("0").join("");
+        return addition(prev, currNum);
+    }, "")
+    return result;
 }
 
 export default multiplication;
